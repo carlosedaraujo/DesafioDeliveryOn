ebb7ba31f3838fc90037b3166b406a8b5c3450bd
Carlos Eduardo Araujo <carlosed.araujo@gmail.com>
2024-06-06 03:06:52 -0300

    melhorias

--- desafio/Program.cs
+++ desafio/Program.cs
@@ -1,36 +1,40 @@
 ﻿using System;
 
+public class Desafio {
+    public static void Main(string[] args) {
 
-Console.WriteLine("Quantos elementos no array?");
-int tamanhoArray = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Digite dois numeros separados por espaço (sendo o primeiro o tamanho do array e o segundo o numero do item que não será pago)");
+        string[] nk = Console.ReadLine().Split(' ');
 
-int[] array = new int[tamanhoArray];
+        List<int> lista = new List<int>();
+        foreach (var item in nk)
+        {
+            lista.Add(Convert.ToInt32(item));
+        }
 
-for (int i=0; i < tamanhoArray; i++) 
-{
-    Console.WriteLine($"Digite o elemento {i+1}");
-    array[i] = Convert.ToInt32(Console.ReadLine());
+        int k = lista[1];
+
+        int tamanhoArray = Convert.ToInt32(lista[0]);
+
+        int[] array = new int[tamanhoArray];
+
+        for (int i=0; i < tamanhoArray; i++) 
+        {
+            Console.WriteLine($"Digite o elemento {i+1}");
+            array[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        int b = array[k];
+
+        Console.WriteLine(b);
+
+    static void resposta (List<int> array, int k, int b) 
+    {
+            int soma = (array.Sum() - b) / 2;
+            
+
+
+    }
+   }
 }
-
-Console.WriteLine("Elementos do array:");
-foreach (var item in array)
-{
-    Console.WriteLine(item);
-}
-
-Console.WriteLine("Digite dois numeros separados por espaço");
-string[] nk = Console.ReadLine().Split(' ');
-List<int> lista = new List<int>();
-foreach (var item in nk)
-{
-    lista.Add(Convert.ToInt32(item));
-}
-
-foreach (var i in lista) {
-    Console.WriteLine(i);
-}
-   
-
-
-
 
