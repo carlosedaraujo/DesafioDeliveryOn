2036c576f42ef906ef3d8145b4a0d891f44c766c
Carlos Eduardo Araujo <carlosed.araujo@gmail.com>
2024-06-06 02:27:55 -0300

    initial commit

---
+++ desafio/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+Console.WriteLine("Quantos elementos no array?");
+int tamanhoArray = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[tamanhoArray];
+
+for (int i=0; i < tamanhoArray; i++) 
+{
+    Console.WriteLine($"Digite o elemento {i+1}");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine("Elementos do array:");
+foreach (var item in array)
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine("Digite dois numeros separados por espaço");
+string[] nk = Console.ReadLine().Split(' ');
+List<int> lista = new List<int>();
+foreach (var item in nk)
+{
+    lista.Add(Convert.ToInt32(item));
+}
+
+foreach (var i in lista) {
+    Console.WriteLine(i);
+}
+   
+
+
+
+
